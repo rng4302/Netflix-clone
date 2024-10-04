6c7dbeb970624548c46a80aa5802ae927418979c
Raj <rajgariwala40302@gmail.com>
2024-10-05 02:47:57 +1000

    Test 5

--- scripts/Terraform/kube-cluster.tf
+++ scripts/Terraform/kube-cluster.tf
@@ -7,8 +7,8 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_B2s"
+    node_count = 2
+    vm_size    = "Standard_D2s_v3"
   }
 
 
